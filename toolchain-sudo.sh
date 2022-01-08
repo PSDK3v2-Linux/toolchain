16fc338f42525c256b92316d5610ca5f584994aa
rhynec <rhynec@pm.me>
2022-01-08 01:53:09 -0600

    Build script tweaks
    - Removed MinGW (Windows) build (non-tested for PSDK3v2).
    - Update environment variables for sudo script.

--- toolchain-sudo.sh
+++ toolchain-sudo.sh
@@ -5,8 +5,9 @@
 cd "`dirname $0`" || { echo "ERROR: Could not enter the ps3toolchain directory."; exit 1; }
 
 ## Set up the environment.
-export PS3DEV=/usr/local/ps3dev
-export PSL1GHT=$PS3DEV
+export PS3SDK=/usr/local/PSDK3v2
+export PS3DEV=$PS3SDK/ps3dev
+export PSL1GHT=$PS3SDK/psl1ght
 
 ## Set up the path.
 export PATH=$PATH:$PS3DEV/bin
